7962d3efe899d3c728e61975e1c84d3044933119
Benedict Mwanga <benedict@Benedicts-MacBook-Air.local>
2017-03-03 05:35:09 +0300

    add some style here and there

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,6 +8,12 @@
         
         <div class="container">
             <h1>Hello World from the MacBookAir</h1>
+            <div class="col-md-8">
+                <h2>I am so glad to day brothers</h2>
+            </div>
+            <div class="col-md-4">
+                <h3>We had this dream of codding anywhere but now we can do more that that</h3>
+            </div>
         </div>
         
         <script type="text/javascript" src="/js/app.js"></script>
