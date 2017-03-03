1f463f46f21b56df056c150a1997c7ffe7055559
Benedict Mwanga <benedict@Benedicts-MacBook-Air.local>
2017-03-03 05:57:05 +0300

    add with sisters

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,7 +9,7 @@
         <div class="container">
             <h1>Hello World from the MacBookAir</h1>
             <div class="col-md-8 reds">
-                <h2>I am so glad to day brothers</h2>
+                <h2>I am so glad to day brothers and sisters</h2>
             </div>
             <div class="col-md-4">
                 <h3>We had this dream of codding anywhere but now we can do more that that</h3>
